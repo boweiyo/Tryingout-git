062ac951210e76c9c56a242caf0e40198b0c3f4f
boweiyo <bowei@chotot.vn>
2015-04-02 09:57:08 +0700

    Added Entire Project

--- app/src/androidTest/java/com/bowei/tryingoutgit/ApplicationTest.java
+++ app/src/androidTest/java/com/bowei/tryingoutgit/ApplicationTest.java
@@ -1,13 +1,40 @@
 package com.bowei.tryingoutgit;
 
 import android.app.Application;
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.assertion.ViewAssertions;
+import android.test.ActivityInstrumentationTestCase2;
 import android.test.ApplicationTestCase;
+
+import junit.framework.TestResult;
+
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.assertThat;
+import static android.support.test.espresso.matcher.ViewMatchers.isClickable;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
-public class ApplicationTest extends ApplicationTestCase<Application> {
+public class ApplicationTest extends ActivityInstrumentationTestCase2<MainActivity> {
+
     public ApplicationTest() {
-        super(Application.class);
+        super(MainActivity.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        getActivity();
+    }
+
+    public void test() {
+        onView(withId(R.id.btnChangeText)).perform(click());
+        onView(withId(R.id.tvMessage)).check(ViewAssertions.matches(withText("Good Bye World")));
     }
 }
